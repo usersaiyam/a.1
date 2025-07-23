fbb13fdc409d57766d30c861b69fe2e2c90fca83
usersaiyam <saiyamt08@gmail.com>
2025-07-23 11:51:05 +0530

    Add files via upload

---
+++ a.1 .py
@@ -0,0 +1,24 @@
+'''
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+print("\nResults:")
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+print(f"Division: {num1} / {num2} = {division}")
+'''
+
+'''
+first_name = input("Enter your 1 name: ")
+last_name = input("Enter your 2 name: ")
+
+full_name = first_name + " " + last_name
+
+print(f"hey {full_name}, welcome!") 
+'''
